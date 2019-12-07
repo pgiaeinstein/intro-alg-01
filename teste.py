4c2cdd4d2b309ab5ffb063fd11360800aa78b041
Fernando Mainetti Secol <fmsecol@gmail.com>
2019-12-07 10:57:52 -0300

    init

---
+++ teste.py
@@ -0,0 +1,14 @@
+import math
+
+var_1 = 33
+var_2 = 22
+var_3 = 84
+
+soma = var_1 + var_2 + var_3
+
+lista = [2, 5, 6, 8, 33, 45, 123, 999]
+
+lista_2 = list()
+
+for item in lista:
+    lista_2.append(item)
